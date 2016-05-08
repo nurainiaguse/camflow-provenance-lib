1539d9f7265f78e81db5d15ccada47e4b10bbc05
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2016-05-08 21:15:08 +0200

    simplify the example code

--- example/audit.c
+++ example/audit.c
@@ -41,75 +41,65 @@ void _init_logs( void ){
 
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
-void write_to_log(const char* fmt, ...){
-  char tmp[10192];
-	va_list args;
-	va_start(args, fmt);
-  vsprintf(tmp, fmt, args);
-	va_end(args);
-  pthread_mutex_lock(&mut);
-  simplog.writeLog(SIMPLOG_INFO, tmp);
-  pthread_mutex_unlock(&mut);
-}
+static __thread char buffer[10192]; // check the size
 
 void init( void ){
   pid_t tid = gettid();
-  write_to_log("audit writer thread, tid:%ld", tid);
+  simplog.writeLog(SIMPLOG_INFO, "audit writer thread, tid:%ld", tid);
 }
 
-static __thread char buffer[6144]; // check the size
 
 void log_str(struct str_struct* data){
-  write_to_log("%s", str_msg_to_json(buffer, data));
+  simplog.writeLog(SIMPLOG_INFO, str_msg_to_json(buffer, data));
 }
 
 void log_link(struct link_struct* link){
-  write_to_log("%s", link_to_json(buffer, link));
+  simplog.writeLog(SIMPLOG_INFO, link_to_json(buffer, link));
 }
 
 void log_unlink(struct unlink_struct* unlink){
-  write_to_log("%s", unlink_to_json(buffer, unlink));
+  simplog.writeLog(SIMPLOG_INFO, unlink_to_json(buffer, unlink));
 }
 
 void log_edge(struct edge_struct* edge){
-    write_to_log("%s", edge_to_json(buffer, edge));
+  simplog.writeLog(SIMPLOG_INFO, edge_to_json(buffer, edge));
 }
 
 void log_task(struct task_prov_struct* task){
-  write_to_log("%s", task_to_json(buffer, task));
+  simplog.writeLog(SIMPLOG_INFO, task_to_json(buffer, task));
 }
 
 void log_inode(struct inode_prov_struct* inode){
-    write_to_log("%s", inode_to_json(buffer, inode));
+  simplog.writeLog(SIMPLOG_INFO, inode_to_json(buffer, inode));
 }
 
 void log_disc(struct disc_node_struct* node){
-    write_to_log("%s", disc_to_json(buffer, node));
+  simplog.writeLog(SIMPLOG_INFO, disc_to_json(buffer, node));
 }
 
 void log_msg(struct msg_msg_struct* msg){
-    write_to_log("%s", msg_to_json(buffer, msg));
+  simplog.writeLog(SIMPLOG_INFO, msg_to_json(buffer, msg));
 }
 
 void log_shm(struct shm_struct* shm){
-    write_to_log("%s", shm_to_json(buffer, shm));
+  simplog.writeLog(SIMPLOG_INFO, shm_to_json(buffer, shm));
 }
 
 
 void log_sock(struct sock_struct* sock){
-    write_to_log("%s", sock_to_json(buffer, sock));
+  simplog.writeLog(SIMPLOG_INFO, sock_to_json(buffer, sock));
 }
 
 void log_address(struct address_struct* address){
-    write_to_log("%s", addr_to_json(buffer, address));
+  simplog.writeLog(SIMPLOG_INFO, addr_to_json(buffer, address));
 }
 
 void log_file_name(struct file_name_struct* f_name){
-    write_to_log("%s", pathname_to_json(buffer, f_name));
+  simplog.writeLog(SIMPLOG_INFO, pathname_to_json(buffer, f_name));
 }
 
 void log_ifc(struct ifc_context_struct* ifc){
-    write_to_log("%s", ifc_to_json(buffer, ifc));
+  simplog.writeLog(SIMPLOG_INFO, ifc_to_json(buffer, ifc));
 }
 
 struct provenance_ops ops = {
@@ -133,7 +123,7 @@ int main(void){
   int rc;
   //hostid = gethostid();
 	_init_logs();
-  simplog.writeLog(SIMPLOG_INFO, "audit process pid: %d", getpid());
+  simplog.writeLog(SIMPLOG_INFO, "audit process pid: %ld", getpid());
   rc = provenance_register(&ops);
   if(rc){
     simplog.writeLog(SIMPLOG_ERROR, "Failed registering audit operation.");
