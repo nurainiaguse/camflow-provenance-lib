fcbd788cc941205cea5721bf529792e19a64569d
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2016-08-23 21:15:45 +0200

    update macro name

--- src/provenancefilter.c
+++ src/provenancefilter.c
@@ -61,25 +61,25 @@ static inline int __provenance_get_filter( const char* file, uint32_t* filter ){
   return 0;
 }
 
-#define define_change_filter_fcn(fcn_name, add, file) int fcn_name ( uint32_t filter ){return __provenance_change_filter(add, file, filter);}
-#define define_get_filter_fcn(fcn_name, file) int fcn_name ( uint32_t* filter ){ return __provenance_get_filter( file, filter );}
+#define declare_change_filter_fcn(fcn_name, add, file) int fcn_name ( uint32_t filter ){return __provenance_change_filter(add, file, filter);}
+#define declare_get_filter_fcn(fcn_name, file) int fcn_name ( uint32_t* filter ){ return __provenance_get_filter( file, filter );}
 
 // node filter
-define_change_filter_fcn(provenance_add_node_filter, true, PROV_NODE_FILTER_FILE);
-define_change_filter_fcn(provenance_remove_node_filter, false, PROV_NODE_FILTER_FILE);
-define_get_filter_fcn(provenance_get_node_filter, PROV_NODE_FILTER_FILE);
+declare_change_filter_fcn(provenance_add_node_filter, true, PROV_NODE_FILTER_FILE);
+declare_change_filter_fcn(provenance_remove_node_filter, false, PROV_NODE_FILTER_FILE);
+declare_get_filter_fcn(provenance_get_node_filter, PROV_NODE_FILTER_FILE);
 
 // propagate node filter
-define_change_filter_fcn(provenance_add_propagate_node_filter, true, PROV_PROPAGATE_NODE_FILTER_FILE);
-define_change_filter_fcn(provenance_remove_propagate_node_filter, false, PROV_PROPAGATE_NODE_FILTER_FILE);
-define_get_filter_fcn(provenance_get_propagate_node_filter, PROV_PROPAGATE_NODE_FILTER_FILE);
+declare_change_filter_fcn(provenance_add_propagate_node_filter, true, PROV_PROPAGATE_NODE_FILTER_FILE);
+declare_change_filter_fcn(provenance_remove_propagate_node_filter, false, PROV_PROPAGATE_NODE_FILTER_FILE);
+declare_get_filter_fcn(provenance_get_propagate_node_filter, PROV_PROPAGATE_NODE_FILTER_FILE);
 
 // relation filter
-define_change_filter_fcn(provenance_add_relation_filter, true, PROV_RELATION_FILTER_FILE);
-define_change_filter_fcn(provenance_remove_relation_filter, false, PROV_RELATION_FILTER_FILE);
-define_get_filter_fcn(provenance_get_relation_filter, PROV_RELATION_FILTER_FILE);
+declare_change_filter_fcn(provenance_add_relation_filter, true, PROV_RELATION_FILTER_FILE);
+declare_change_filter_fcn(provenance_remove_relation_filter, false, PROV_RELATION_FILTER_FILE);
+declare_get_filter_fcn(provenance_get_relation_filter, PROV_RELATION_FILTER_FILE);
 
 // propagate relation filter
-define_change_filter_fcn(provenance_add_propagate_relation_filter, true, PROV_PROPAGATE_RELATION_FILTER_FILE);
-define_change_filter_fcn(provenance_remove_propagate_relation_filter, false, PROV_PROPAGATE_RELATION_FILTER_FILE);
-define_get_filter_fcn(provenance_get_propagate_relation_filter, PROV_PROPAGATE_RELATION_FILTER_FILE);
+declare_change_filter_fcn(provenance_add_propagate_relation_filter, true, PROV_PROPAGATE_RELATION_FILTER_FILE);
+declare_change_filter_fcn(provenance_remove_propagate_relation_filter, false, PROV_PROPAGATE_RELATION_FILTER_FILE);
+declare_get_filter_fcn(provenance_get_propagate_relation_filter, PROV_PROPAGATE_RELATION_FILTER_FILE);
