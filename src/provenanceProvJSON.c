8252186962c8f57dc122d6dbb2870c6e5aeccce3
nurainiaguse <aguse2@illinois.edu>
2017-07-09 09:05:51 -0500

    adding function

--- src/provenanceProvJSON.c
+++ src/provenanceProvJSON.c
@@ -537,6 +537,13 @@ char* disc_to_json(struct disc_node_struct* n){
   return buffer;
 }
 
+void fromDocker(char* secctx){
+  if (strncmp(secctx, "system_u:object_r:container_file_t:s0:c355,c409", strlen("system_u:object_r:container_file_t:s0:c355,c409")) == 0)
+    __add_string_attribute("from_docker", "true", true);
+  else if (strncmp(secctx, "system_u:system_r:container_t:s0:c355,c409", strlen("system_u:system_r:container_t:s0:c355,c409")) == 0)
+    __add_string_attribute("from_docker", "true", true);
+}
+
 char* task_to_json(struct task_prov_struct* n){
   char tmp[33];
   char secctx[PATH_MAX];
@@ -555,7 +562,7 @@ char* task_to_json(struct task_prov_struct* n){
   __add_uint32_attribute("cf:net_namespace", n->netns, true);
   __add_uint32_attribute("cf:cgroup_namespace", n->cgroupns, true);
   __add_string_attribute("cf:secctx", secctx, true);
-  __add_string_attribute("fromDocker", "true", true);
+  fromDocker(secctx);
   __add_label_attribute("task", utoa(n->identifier.node_id.version, tmp, DECIMAL), true);
   __close_json_entry(buffer);
   return buffer;
@@ -616,7 +623,7 @@ char* inode_to_json(struct inode_prov_struct* n){
   __add_uint32_attribute("cf:gid", n->gid, true);
   __add_uint32hex_attribute("cf:mode", n->mode, true);
   __add_string_attribute("cf:secctx", secctx, true);
-  __add_string_attribute("fromDocker", "true", true);
+  fromDocker(secctx);
   __add_uint32_attribute("cf:ino", n->ino, true);
   __add_string_attribute("cf:uuid", uuid_to_str(n->sb_uuid, uuid, UUID_STR_SIZE), true);
   __add_label_attribute(node_str(n->identifier.node_id.type), utoa(n->identifier.node_id.version, tmp, DECIMAL), true);
